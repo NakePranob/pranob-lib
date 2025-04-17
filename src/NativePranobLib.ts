5e48dad765e387106f9c4d8a7037cbee7115d405
NakePranob <pranob.onphaeng@gmail.com>
2025-04-17 13:13:17 +0700

    feat: make test component

--- src/NativePranobLib.ts
+++ src/NativePranobLib.ts
@@ -3,6 +3,7 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
+  testComponent(): any;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('PranobLib');
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import PranobLib from './NativePranobLib';
+import { View, Text } from 'react-native';
 
 export function multiply(a: number, b: number): number {
   return PranobLib.multiply(a, b);
+}
+
+export function testComponent() {
+  return (
+    <View>
+      <Text>Test Component</Text>
+    </View>
+  );
 }
